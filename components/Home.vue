ea778cab926f5bd466c810ccb9f94f6f91d11d80
yrung <yrungen@163.com>
2018-08-02 17:18:51 +0800

    2018-08-02 自定义组件

---
+++ components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>{{msg}}</h3>
+</template>
+
+<script>
+    alert(1);
+    export default {
+        name: "Home",
+        data() {
+            return {
+                msg: "这是首页"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/User.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>{{msg}}</h3>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        data() {
+            return {
+                msg: "这是用户"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
